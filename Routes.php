b530b57cd8fbe6657712977f4d744afc73ee6140
denisarambasic <denis.arambasic@gmail.com>
2018-03-08 00:10:47 +0100

    added the articles route to get all articles

--- Routes.php
+++ Routes.php
@@ -7,6 +7,8 @@ $routes = new Route();
 /*=== Homepage routes ===*/
 $routes->setRoute('GET', '/', 'HomepageController', 'index', []);
 
+/*=== Articles routes ===*/
+$routes->setRoute('GET', '/articles', 'ArticleController', 'index', []);
 
 /*=== Call the controller if the request match an existing route ===*/
 $routes->callController();
