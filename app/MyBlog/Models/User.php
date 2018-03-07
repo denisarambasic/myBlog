304d863092ec83eeb42d0ba9ae3b5c8a37371554
denisarambasic <denis.arambasic@gmail.com>
2018-03-07 22:06:35 +0100

    mapped my users table into a the User class

---
+++ app/MyBlog/Models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MyBlog\Models;
+
+class User
+{
+	private $id;
+	private $email;
+	private $password;
+	private $created_at;
+	
+	/*=== Getter for Id ===*/
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	/*=== Getter and Setter for Email ===*/
+	public function getEmail()
+	{
+		return $this->email;
+	}
+	
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+	
+	/*=== Getter and Setter for Password ===*/
+	public function getPassword()
+	{
+		return $this->password;
+	}
+	
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+	/*=== Getter and Setter for Created_at ===*/
+	public function getCreated_at()
+	{
+		return $this->created_at;
+	}
+	
+	public function setCreated_at($created_at)
+	{
+		$this->created_at = $created_at;
+	}
+	
+}
