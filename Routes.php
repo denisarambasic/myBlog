b3fd025cf87635d3b019c289b3fe37ab144d81ad
denisarambasic <denis.arambasic@gmail.com>
2018-03-08 17:49:48 +0100

    added a new route that handles the get request on the /register path

--- Routes.php
+++ Routes.php
@@ -10,5 +10,8 @@ $routes->setRoute('GET', '/', 'HomepageController', 'index', []);
 /*=== Articles routes ===*/
 $routes->setRoute('GET', '/articles', 'ArticleController', 'index', []);
 
+/*=== Register routes ===*/
+$routes->setRoute('GET', '/register', 'RegisterController', 'index', []);
+
 /*=== Call the controller if the request match an existing route ===*/
 $routes->callController();
