871b34b9e1c8e78f86fa73261f6905d00beb327f
denisarambasic <denis.arambasic@gmail.com>
2018-03-09 19:53:35 +0100

    added an authentication layer

---
+++ app/MyBlog/Helpers/Authentication.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyBlog\Helpers;
+
+use \Firebase\JWT\JWT;
+
+class Authentication
+{
+	
+	public static function requireAuth()
+	{
+		//1.get the access_token from the request
+		$access_token = substr(apache_request_headers()["Authorization"], 7);
+		
+		//2. check if token exists if not protect te resurs
+		if(!$access_token){
+			$message = ["error" => "you don't send any access_token"];
+			http_response_code(403);
+			header('Content-type: application/json');
+			echo json_encode($message);
+			exit;
+		}
+		
+		//3. check if the token is valid if not valid protect the resurs
+		try{
+			$decoded = JWT::decode($access_token, getenv('SECRET_KEY'), array('HS256'));
+			return;
+		}catch(\Exception $e){
+			$message = ["error" => "your access_token has expired"];
+			http_response_code(403);
+			header('Content-type: application/json');
+			echo json_encode($message);
+			exit;
+		}
+
+	}
+	
+}
