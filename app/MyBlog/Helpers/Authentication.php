26a9f79310e29891fcc7dfb5133d957ca4bf1996
denisarambasic <denis.arambasic@gmail.com>
2018-03-12 15:54:07 +0100

    Added some modification to send back the user id from the decoded json web token

--- app/MyBlog/Helpers/Authentication.php
+++ app/MyBlog/Helpers/Authentication.php
@@ -10,8 +10,10 @@ class Authentication
 	public static function requireAuth()
 	{
 		//1.get the access_token from the request
-		$access_token = substr(apache_request_headers()["Authorization"], 7);
-		
+		$access_token = null;
+		if(isset(apache_request_headers()["Authorization"])){
+			$access_token = substr(apache_request_headers()["Authorization"], 7);
+		}
 		//2. check if token exists if not protect te resurs
 		if(!$access_token){
 			$message = ["error" => "you don't send any access_token"];
@@ -24,9 +26,10 @@ class Authentication
 		//3. check if the token is valid if not valid protect the resurs
 		try{
 			$decoded = JWT::decode($access_token, getenv('SECRET_KEY'), array('HS256'));
-			return;
+			//return the user id of the current user
+			return $decoded->sub;
 		}catch(\Exception $e){
-			$message = ["error" => "your access_token has expired"];
+			$message = ["error" => "your access_token has expired, login again"];
 			http_response_code(403);
 			header('Content-type: application/json');
 			echo json_encode($message);
