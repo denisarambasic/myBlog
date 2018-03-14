ab8ce6fd4d821666b892c25b2b9e4512e18286e9
denisarambasic <denis.arambasic@gmail.com>
2018-03-14 13:10:30 +0100

    added a new route for creating a new article

--- Routes.php
+++ Routes.php
@@ -16,6 +16,7 @@ $routes->setRoute('GET', '/articles/page', 'ArticleController', 'getPerPage', ['
 /*=== User article routes ===*/
 $routes->setRoute('GET', '/my-articles', 'UserController', 'index', []);
 $routes->setRoute('GET', '/my-articles/create', 'UserController', 'createArticleInfo', []);
+$routes->setRoute('POST', '/my-articles/create', 'UserController', 'createArticle', []);
 $routes->setRoute('OPTIONS', '/my-articles', 'UserController', 'index', []);
 $routes->setRoute('GET', '/check-user', 'UserController', 'checkUser', []);
 $routes->setRoute('OPTIONS', '/check-user', 'UserController', 'checkUser', []);
